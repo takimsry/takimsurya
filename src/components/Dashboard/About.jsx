bfdff99047c6010b382a851b3de2c1c7e7a145a0
M. Mustakim Surya <muhammadmustakimsurya@gmail.com>
2024-08-27 15:52:44 +0700

    change cv

--- src/components/Dashboard/About.jsx
+++ src/components/Dashboard/About.jsx
@@ -27,7 +27,7 @@ const About = () => {
                 Let's collaborate and bring your <strong>ideas</strong> to life!
               </p>
             </div>
-            <a href="https://drive.google.com/file/d/1yWCF2K_igEUal3_eM1JZHQBdO1lXA_LQ/view?usp=sharing" className="btn btn--med btn--theme dynamicBgClr" target="_blank" rel="noreferrer"
+            <a href="https://drive.google.com/file/d/1fD7lv5EwttAZj3zBelm-eigVS9GrFXhi/view?usp=sharing" className="btn btn--med btn--theme dynamicBgClr" target="_blank" rel="noreferrer"
             >My CV</a
             >
           </div>
